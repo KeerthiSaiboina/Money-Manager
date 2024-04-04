6bf3e2ddcaf7e232d09bee3228717ec3a237b23a
keerthi <keerthisaiboina@gmail.com>
2024-04-04 06:45:49 +0000

    solution

---
+++ src/components/MoneyManager/index.js
@@ -0,0 +1,89 @@
+import {Component} from 'react'
+import {v4} from 'uuid'
+import './index.css'
+
+import MoneyDetails from '../MoneyDetails'
+
+const transactionTypeOptions = [
+  {
+    optionId: 'INCOME',
+    displayText: 'Income',
+  },
+  {
+    optionId: 'EXPENSES',
+    displayText: 'Expenses',
+  },
+]
+
+// Write your code here
+class MoneyManager extends Component {
+  state = {
+    titleInput: '',
+    amountInput: '',
+    transactionList: [],
+    optionId: transactionTypeOptions[0].optionId,
+  }
+  getExpenses = () => {
+    const {transactionList} = this.state
+    let expensesAmount = 0
+
+    transactionList.forEach(eachTransaction => {
+      if (eachTransaction.type === transactionTypeOptions[1].displayText) {
+        expensesAmount += eachTransaction.amount
+      }
+      return expensesAmount
+    })
+  }
+
+  getIncome = () => {
+    const {transactionList} = this.state
+    let incomeAmount = 0
+    transactionList.forEach(eachTransaction => {
+      if (eachTransaction.type === transactionTypeOptions[0].displayText) {
+        incomeAmount += eachTransaction.amount
+      }
+      return incomeAmount
+    })
+  }
+
+  getBalance = () => {
+    const {transactionList} = this.state
+    let balanceAmount = 0
+    let incomeAmount = 0
+    let expensesAmount = 0
+
+    transactionList.forEach(eachTransaction => {
+      if (eachTransaction.type === transactionTypeOptions[0].displayText) {
+        incomeAmount += eachTransaction.amount
+      } else {
+        expensesAmount += eachTransaction.amount
+      }
+    })
+    balanceAmount = incomeAmount - expensesAmount
+    return balanceAmount
+  }
+
+  render() {
+    const balanceAmount = this.getBalance()
+    const incomeAmount = this.getIncome()
+    const expensesAmount = this.getExpenses()
+    return (
+      <div className="bg-container">
+        <div className="money-manager-container">
+          <h1 className="name-heading">Hi, Richard</h1>
+          <p className="description">
+            Welcome back to your <span className="span">Money Manager</span>
+          </p>
+        </div>
+
+        <MoneyDetails
+          balanceAmount={balanceAmount}
+          incomeAmount={incomeAmount}
+          expensesAmount={expensesAmount}
+        />
+      </div>
+    )
+  }
+}
+
+export default MoneyManager
